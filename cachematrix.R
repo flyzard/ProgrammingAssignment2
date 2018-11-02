989958dabeb75dbcd9fbd30f07d30f5c2a682591
Joao Felizardo <joaofelizardo@ymail.com>
2018-11-02 20:35:38 +0100

    Implement functions

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The following functions are an exercise to introduce the operator `<<-`
+## which assigns the value to an object in a environment different from
+## the current environment. 
 
-## Write a short comment describing this function
+## makeCacheMatrix function returns a list representing an object with 
+## the several methods to control values of the inner properties
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  	i <- NULL
+  	set <- function(y) {
+  		message("setting cached data")
+  	  x <<- y
+  	  i <<- NULL
+  	}
+  	get <- function() x
+  	setinverse <- function(inverse) i <<- inverse
+  	getinverse <- function() i
+  	list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## In cacheSolve function gets a makeCacheMatrix list and checks if the
+## inverse matrix value is already calculated in the give list object. 
+## In case this is already calculated, returns the value, if not, 
+## gets the data from the given matrix, inverts it, and call 
+## setinverse on the list object to store the value in x.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  	i <- x$getinverse()
+  	if(!is.null(i)) {
+		message("getting cached data")
+		return(i)
+  	}
+  	data <- x$get()
+  	i <- solve(data)
+  	x$setinverse(i)
+  	i
 }
